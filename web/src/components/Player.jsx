d305d1eb282a7e8dbc6a7dcf2472679c9be0cf29
Vlad Gudov <vlad.gudov@inbox.ru>
2022-03-14 20:09:37 +0400

    Player component

---
+++ web/src/components/Player.jsx
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from "react"
+import ReactPlayer from "react-player"
+
+const HLSPlayer = () => {
+  const player = useRef(null)
+  const [levels, setLevels] = useState([])
+
+  const handleReady = (player) => {
+    const hls = player.getInternalPlayer("hls")
+    setLevels(hls.levels)
+  }
+
+  const handleLevelClick = (level) => () => {
+    player.current.getInternalPlayer("hls").currentLevel = level
+  }
+
+  return (
+    <>
+      <div>
+        <ReactPlayer
+          ref={player}
+          onReady={handleReady}
+          className="react-player"
+          url=""
+          width="100%"
+          height="100%"
+          controls={true}
+        />
+      </div>
+      <div>
+        {levels.map((l, index) => <div onClick={handleLevelClick(index)} key={l.name}>{l.height}</div>)}
+      </div>
+    </>
+  )
+}
+
+export default HLSPlayer
